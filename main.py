23c8832b544246dfdb5cd03bcbadaea26c56df32
PranavDataFootballer <128952502+PranavDataFootballer@users.noreply.github.com>
2026-08-04 00:36:41 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import turtle 
+import pandas as pd
+
+screen = turtle.Screen()
+screen.title("Name the States")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pd.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
+
+    if answer_state == "Exit":
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item() , state_data.y.item())
+        t.write(answer_state)
